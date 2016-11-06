070e9fdf23ce7ba5c524d2e6686659ec12cee84f
case <case@casesandberg.com>
2016-11-06 00:55:04 -0700

    Tests

---
+++ __tests__/index.spec.js
@@ -0,0 +1,46 @@
+/* global describe, test, expect, jest, beforeEach */
+
+import { scopeStateToSelectors } from '../src'
+
+
+describe('scopeStateToSelectors', () => {
+  test('scopes state down to selector functions', () => {
+    const store = {
+      ui: {
+        filter: 'active',
+      },
+    }
+    const getFilterState = (state) => {
+      return state.filter
+    }
+    const selectors = scopeStateToSelectors({
+      ui: { getFilterState },
+    })
+
+    expect(selectors.getFilterState(store)).toBe('active')
+  })
+
+  test('scopes deepy nested state down to selector functions', () => {
+    const store = {
+      messages: {
+        ui: {
+          tabBar: {
+            filter: 'active',
+          },
+        },
+      },
+    }
+    const getFilterState = (state) => {
+      return state.filter
+    }
+    const selectors = scopeStateToSelectors({
+      messages: scopeStateToSelectors({
+        ui: scopeStateToSelectors({
+          tabBar: { getFilterState },
+        }),
+      }),
+    })
+
+    expect(selectors.getFilterState(store)).toBe('active')
+  })
+})
