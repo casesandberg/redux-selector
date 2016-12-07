47904bfa79327ebeac5f8a8174edf01fd9a1d560
case <case@casesandberg.com>
2016-12-07 05:26:00 -0800

    Add Second Param for Cross-Selectors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import _ from 'lodash'
 
-export const scopeStateToSelectors = selectorsMap =>
+export const scopeStateToSelectors = (selectorsMap, crossSelectors = {}) =>
   _.reduce(selectorsMap, (allScopedSelectors, selectors, scope) => (
     _.reduce(selectors, (scopedSelectors, selector, selectorName) => ({
       ...scopedSelectors,
       [selectorName]: (state, ...rest) => selector(state[scope], ...rest),
     }), allScopedSelectors)
-  ), {})
+  ), crossSelectors)
